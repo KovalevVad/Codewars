9e5e16e150598bb4b21271922fc0a0651f93563d
Vadim Kovalev <nikas.367@mail.ru>
2024-01-19 22:09:35 +0300

    two review

--- 2/app.js
+++ 2/app.js
@@ -1,8 +1,9 @@
 const initialArray = ['Say', 'hello', 'to', 'world', 'javascript', 'students']
 
-const result = initialArray.forEach((item,index,initialArray) => {
+initialArray.forEach((item,index,initialArray) => {
   if (item.length < 6) {
     delete initialArray[index]
   }
-})// Реализовать эту функцию
-console.log(initialArray) // Выведет ['javascript', 'students']
+})
+const result = initialArray.filter(( element ) => element !== undefined)
+console.log(result) // Выведет ['javascript', 'students']
