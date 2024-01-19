43ad148b9dd474610838a368a0b00458ef0bed61
Vadim Kovalev <144165066+KovalevVad@users.noreply.github.com>
2024-01-19 22:16:37 +0300

    Delete 20 directory

--- 20/app.js
+++
@@ -1,5 +0,0 @@
-const initialArray = [19, 1, 3, 4, 5, 6, 20, 8]
-
-const result = initialArray.filter((el) => initialArray.indexOf(el)%2===0) // Реализовать эту функцию
-
-console.log(result) // Выводит [19, 3, 5, 20]
